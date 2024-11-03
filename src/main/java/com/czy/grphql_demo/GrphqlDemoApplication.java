c2676868f2e7873b649bb0b9389988fd3e2ac864
chenzhiyuan <zhiyuan2.chen@ly.com>
2024-11-03 16:36:59 +0800

    init

---
+++ src/main/java/com/czy/grphql_demo/GrphqlDemoApplication.java
@@ -0,0 +1,13 @@
+package com.czy.grphql_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrphqlDemoApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(GrphqlDemoApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/czy/grphql_demo/GrphqlDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.czy.grphql_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrphqlDemoApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
